6a65637fbae24ea03de14e7bfe2a49eb3da6991e
siqizheng <cn.siqizheng@gmail.com>
2023-11-23 14:16:28 +0100

    Rest Controller GET

---
+++ src/main/java/com/example/mongodb/Character.java
@@ -0,0 +1,4 @@
+package com.example.mongodb;
+
+public record Character(String id,String name,int age,String profession) {
+}
---
+++ src/main/java/com/example/mongodb/PersonController.java
@@ -0,0 +1,9 @@
+package com.example.mongodb;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
+public class PersonController {
+}
