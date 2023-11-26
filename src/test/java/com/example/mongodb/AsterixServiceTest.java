1714357d08859e8f2db11063f892bb477e290ff7
siqizheng <cn.siqizheng@gmail.com>
2023-11-26 19:04:15 +0100

    Strict Tests

--- src/test/java/com/example/mongodb/AsterixServiceTest.java
+++ src/test/java/com/example/mongodb/AsterixServiceTest.java
@@ -38,6 +38,10 @@ class AsterixServiceTest {
         Character expected = character1;
         verify(asterixRepo).findById("1");
         assertEquals(actual,expected);
+        //strict test:the methode of my mock was called only once,
+//and no other methods was called
+        verify(asterixRepo,times(1)).findById("1");
+        verifyNoMoreInteractions(asterixRepo);
     }
     @Test
     void updateCharacterDTO_whenId1_thenCharacterDTO1(){
@@ -52,14 +56,44 @@ class AsterixServiceTest {
         verify(asterixRepo).findById("1");
         verify(asterixRepo).save(character1);
         assertEquals(expected,actual);
+//strict test:the methode of my mock was called only once,
+//and no other methods was called
+        verify(asterixRepo,times(1)).findById("1");
 
+        verify(asterixRepo,times(1)).save(character1);
+        verifyNoMoreInteractions(asterixRepo);
     }
     @Test
-    void deleteCharacter_whenId1_thenReturn(){
-
+    void deleteCharacter_whenId2_thenSuccessful(){
 
         asterixService.deleteCharacter("2");
         verify(asterixRepo).deleteById("2");
-
+//strict test:the methode of my mock was called only once,
+//and no other methods was called
+        verify(asterixRepo,times(1)).deleteById("2");
+        verifyNoMoreInteractions(asterixRepo);
     }
+    @Test
+    void saveCharacter_whenCharacterDTO_thenCharacter1(){
+        NewCharacterDTO characterDTO = new NewCharacterDTO("a", 100, "Muster");
+
+        Character character1 = new Character("1", characterDTO.name(), characterDTO.age(),characterDTO.occupation());
+        when(asterixRepo.save(character1)).thenReturn(character1);
+        when(idService.randomId()).thenReturn("1");
+
+
+        Character actual= asterixService.saveCharacter(characterDTO);
+        Character expected = new Character("1","a", 100, "Muster");
+
+        verify(asterixRepo).save(character1);
+        verify(idService).randomId();
+        assertEquals(actual,expected);
+//strict test:the methode of my mock was called only once,
+//and no other methods was called
+        verify(asterixRepo,times(1)).save(character1);
+        verifyNoMoreInteractions(asterixRepo);
+        verify(idService,times(1)).randomId();
+        verifyNoMoreInteractions(idService);
+    }
+
 }
